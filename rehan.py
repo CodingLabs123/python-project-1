c7e25e2270306c852f83402a4f96e9463376d709
CodingLabs123 <115024056+CodingLabs123@users.noreply.github.com>
2026-04-02 22:21:57 +0530

    Add files via upload
    This Python-based automation tool is designed to streamline the creation of professional "About Me" drafts. By taking user inputs regarding academic background, technical skills, and professional experience, the script utilizes F-string formatting to generate a cohesive, industry-ready career summary.
    
    This project demonstrates a clean application of User Input Handling and Dynamic String Manipulation in Python.
    
    🛠️ Features
    Interactive CLI: Prompts users for key professional data points including coding languages, experience, and education.
    
    Dynamic Template Engine: Uses a pre-defined logic structure to weave individual data points into a professional narrative.
    
    Versatile Output: Generates a draft suitable for LinkedIn "About" sections, CV summaries, or portfolio introductions.
    
     Technologies Used
    Language: Python 3.x
    
    Key Concepts: Input/Output (I/O) handling, String Formatting (F-strings), Variable Management.

---
+++ rehan.py
@@ -0,0 +1,20 @@
+name =   input("enter your name? ")
+age =  input ("enter your age? ")
+profetion =  input("enter your profetion? ")
+years_of_experience = input(" enter your years of experience? ")
+college_name = input("enter your college name? ")
+coding_languages_known = input(" enter the languages known? ")
+living = input(" enter  where your living? ")
+school = input(" enter your school? ")
+draft_letter = f"{name} is a dedicated {profetion} living in {living}, who is currently pursuing a degree at {college_name}.\nWith over a year of experience in project-based development and technical internships, he has built a strong profession centered on bridging the gap between hardware and intelligent software.\nHis technical toolkit includes a deep command of {coding_languages_known}, which he uses to design machine learning models and optimize complex algorithms.\n{name}'s unique background in {school} allows him to integrate {profetion} with practical engineering solutions, ranging from 3D printing optimizations to real-time data analysis, making him a versatile asset in the evolving field of {profetion}."
+
+
+
+
+print (draft_letter)
+
+
+
+
+
+
